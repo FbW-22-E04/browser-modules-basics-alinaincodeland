109c10cf2feb18f268636cec7a42ce45677a5d1c
dci-student <alinacuznetov@gmail.com>
2022-09-26 12:01:48 +0200

    solution added

---
+++ modules/index.js
@@ -0,0 +1,9 @@
+// Export the hover.js, scroll.js and click.js files and import them into the index.js file
+
+import { hoverHeader } from "./hover.js";
+import { clickImages } from "./click.js";
+import { scrollWindow } from "./scroll.js";
+
+hoverHeader();
+clickImages();
+scrollWindow();
